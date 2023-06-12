cc751fefc212c78ab166e28aa7cb70b695c2035e
kyle-saryg <115197284+kyle-saryg@users.noreply.github.com>
2023-06-11 19:07:02 -0700

    Lazy Loading

---
+++ Day39 (GeometryReader, Lazy Loading)/LazyLoading.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Kyle Sarygin on 6/11/23.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating new object")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0 ..< 100) {
+                    CustomText("Item #\($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

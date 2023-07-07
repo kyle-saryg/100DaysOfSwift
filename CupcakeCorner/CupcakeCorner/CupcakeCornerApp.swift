472121914fc9c32c5b73f7fcfcaeacc4d55a81b7
kyle-saryg <115197284+kyle-saryg@users.noreply.github.com>
2023-07-07 15:57:24 -0700

    Creating a Codable Class

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kyle Sarygin on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

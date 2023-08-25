a5e144cae8732371d8d38f92ae829b8ee1f66665
kyle-saryg <115197284+kyle-saryg@users.noreply.github.com>
2023-08-25 00:29:31 -0700

    Nvm im a mf genius

--- Day 60 Challenge/Day 60 Challenge/ContentView.swift
+++ Day 60 Challenge/Day 60 Challenge/ContentView.swift
@@ -11,36 +11,49 @@ struct ContentView: View {
     @State private var users: [User] = []
     
     var body: some View {
-        VStack {
-            Button("testPrint", action: testPrint)
-            Button("FetchJSON", action: fetchJSON)
+        NavigationView {
+            List(users) { user in
+                HStack {
+                    Image(systemName: "circle.fill")
+                        .foregroundColor(user.isActive ? Color(.systemGreen): Color(.systemRed))
+                    Text(user.name)
+                    Text(user.registered)
+                }
+            }
+            .onAppear(perform: fetchUserData)
         }
-        .padding()
     }
     
-    func testPrint() {
-        print("TESTINGTESTINGTESTING")
-    }
-    
-    func fetchJSON() {
+    func fetchUserData() {
         guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
-            fatalError("Failed to load JSON")
+            fatalError("Error retrieving URL")
         }
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // error will be nil if successfull
+            if let error = error {
+                // if let -> enters code block if non-nil value
+                fatalError("Error: \(error.localizedDescription)")
+            }
+            
             guard let data = data else {
+                // guard let -> enters code block if nil value
+                // redundant code, shouldn't enter this code block since if any errors are present
+                // should be handled above
+                print("Data fetch error, \(error?.localizedDescription ?? "Unkown Error line 33")")
                 return
             }
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
             do {
-                print("LINE 35")
-                let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
-                print("LINE 36")
-                DispatchQueue.main.async {
-                    print("LINE 38")
-                    self.users = decodedResponse.items
-                }
+                users = try decoder.decode([User].self, from: data)
+                return
             } catch {
-                print("Error decoding json")
+                print("Decoding Error: \(error)")
             }
         }.resume()
     }
--- Day 60 Challenge/Day 60 Challenge/User.swift
+++ Day 60 Challenge/Day 60 Challenge/User.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+struct Users: Codable {
+    let users: [User]
+}
+
 struct User: Codable, Identifiable {
     struct Friend: Codable, Identifiable {
         let id: String
@@ -16,12 +20,12 @@ struct User: Codable, Identifiable {
     let id: String
     let isActive: Bool
     let name: String
-//    let age: Int
-//    let company: String
-//    let email: String
-//    let address: String
-//    let about: String
-//    let registered: String
-//    let tags: [String]
+    let age: Int
+    let company: String
+    let email: String
+    let address: String
+    let about: String
+    let registered: String
+    let tags: [String]
     let friends: [Friend]
 }

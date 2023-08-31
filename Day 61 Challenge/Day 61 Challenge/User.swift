32d31cf8ebe57f6b240ac66b69a411debd22f329
kyle-saryg <115197284+kyle-saryg@users.noreply.github.com>
2023-08-30 19:50:07 -0700

    Challenge on pause

--- Day 61 Challenge/Day 61 Challenge/User.swift
+++ Day 61 Challenge/Day 61 Challenge/User.swift
@@ -7,10 +7,6 @@
 
 import Foundation
 
-struct Users: Codable {
-    let users: [User]
-}
-
 struct User: Codable, Identifiable {
     struct Friend: Codable, Identifiable {
         let id: String
@@ -21,11 +17,6 @@ struct User: Codable, Identifiable {
     let isActive: Bool
     let name: String
     let age: Int
-    let company: String
-    let email: String
-    let address: String
-    let about: String
     let registered: String
-    let tags: [String]
     let friends: [Friend]
 }

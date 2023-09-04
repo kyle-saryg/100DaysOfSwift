de25fe743ca3397cb73106b763c2ee7ca4203ed9
kyle-saryg <115197284+kyle-saryg@users.noreply.github.com>
2023-09-04 01:12:21 -0700

    Rolled back

--- Projects/BucketList(14)/BucketList(14)/ContentView.swift
+++ Projects/BucketList(14)/BucketList(14)/ContentView.swift
@@ -5,42 +5,12 @@
 //  Created by Kyle Sarygin on 9/3/23.
 //
 
-import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
-     @State private var isUnlocked = false
-    
     var body: some View {
         VStack {
-            if isUnlocked {
-                Text("Successfully authenticated")
-            } else {
-                Text("Whos this rando")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        // '&error' store error in 'error' (one way binding, take result and store in variable)
-        // $ is used with @State properties and is a two-way binding
-        // Device has biometric capabilities
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                if success {
-                    isUnlocked = true
-                } else {
-                    // Whos this rando
-                }
-            }
-        } else {
-            // no biometrics on device
+            Text("Hello World")
         }
     }
 }

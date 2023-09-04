038ee7723eb64a1273fe1657b3fa3e4c00a33c4a
kyle-saryg <115197284+kyle-saryg@users.noreply.github.com>
2023-09-03 23:23:02 -0700

    Day 68

---
+++ BucketList(14)/BucketList(14)/BucketList_14_App.swift
@@ -0,0 +1,17 @@
+//
+//  BucketList_14_App.swift
+//  BucketList(14)
+//
+//  Created by Kyle Sarygin on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketList_14_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
